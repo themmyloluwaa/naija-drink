8e8455dfbcee262fc6884258815275e7226bdc31
Temiloluwa Ojo <themmyloluwaa@gmail.com>
2019-04-08 03:20:44 +0100

    added an about section and worked on the fixed navbar sliding effect.

--- js/app.js
+++ js/app.js
@@ -1,11 +1,42 @@
-const heading = document.querySelector('.fa-bars');
-const navigation = document.querySelector('.nav');
-
-heading.addEventListener('click', () => {
-    if(navigation.style.display == 'none'){
-        navigation.style.display = 'block';
-    }else{
-        navigation.style.display = 'none';
+class UI {
+    hidePreLoader(){
+        document.querySelector('.preloader').style.display = 'none';
     }
-    
-})
+    showNav() {
+        let navContain = document.querySelector('.nav');
+        navContain.classList.toggle('nav-show');
+    }
+
+    videoControl (){
+         let btn = document.querySelector('.video-switch-btn');
+         if(!btn.classList.contains('btnSlide')){
+             btn.classList.add('btnSlide');
+             document.querySelector('.video-item').pause();
+         }else{
+            btn.classList.remove('btnSlide');
+            document.querySelector('.video-item').play();
+         }
+    }
+}
+// contain video
+
+// windows event listener
+eventL();
+function eventL(){
+    // hide preloader once all images and scripts have loaded
+    const ui = new UI ();
+    window.addEventListener('load', () => {
+        ui.hidePreLoader();
+    })
+
+    // navBtn
+    const navBtn = document.querySelector('.navBtn');
+    navBtn.addEventListener('click', ()=> {
+       ui.showNav();
+    })
+
+    const videoSwitch = document.querySelector('.video-switch');
+    videoSwitch.addEventListener('click', () =>{
+        ui.videoControl(); 
+    })
+}
